c38918e22d2b8b69cbbbd36946d6fcc6f5699089
Rafael S. Silva <103973828+Faeldevs@users.noreply.github.com>
2022-10-31 15:51:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal ({
+    distance: '50px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal('  #armas, #equipes',{delay:200, origin:'top'})
+sr.reveal(' #inicio, #copyright ',{delay:100, origin:'left'})
